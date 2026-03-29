d6cf5f87eca339d3a3980372993ac3f81aaef291
Saurabh <79586784+m4saurabh@users.noreply.github.com>
2026-03-30 00:32:12 +0530

    bring your own

--- oxfmt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "oxfmt"
-
-export default defineConfig({
-  semi: false,
-  sortImports: { newlinesBetween: true },
-  sortTailwindcss: {},
-  sortPackageJson: true,
-})
